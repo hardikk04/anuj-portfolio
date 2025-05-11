dde0ee6a8ba55a3b95fdf9aa573fed86211fef4e
Hardik Sisodiya <hardik04062003@gmail.com>
2025-05-11 12:22:35 +0530

    updated

--- components/work/ReelCard.tsx
+++ components/work/ReelCard.tsx
@@ -1,10 +1,10 @@
 const ReelCard = ({ link, index }: { link: string; index: string }) => {
   return (
     <div
-      className={`reel${index} w-[26vw] max-xl:w-[38vw] max-lg:w-[44vw] max-md:w-full h-[70vh]  overflow-hidden`}
+      className={`reel${index} max-xl:w-[38vw] max-lg:w-[44vw] max-md:w-full h-[70vh]  overflow-hidden`}
     >
       <video
-        className="w-full h-full object-cover"
+        className="w-full h-full object-contain"
         src={link}
         loop
         muted
